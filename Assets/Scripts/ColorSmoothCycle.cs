3b3dcde4ede562fffb441d994a9dac0879bf6bcf
klhaavik <76272131+klhaavik@users.noreply.github.com>
2024-10-15 22:16:40 -0400

    In the process of revamping art

--- Assets/Scripts/ColorSmoothCycle.cs
+++ Assets/Scripts/ColorSmoothCycle.cs
@@ -6,11 +6,16 @@ public class ColorSmoothCycle : MonoBehaviour
 {
     float secondsToCycle;
     public float minSecondsToCycle, maxSecondsToCycle;
+    float offsetMagnitude;
+    public float minOffset, maxOffset;
     SpriteRenderer sr;
     Color startingColor;
     float currentHue;
     public float saturation = 0.2f;
     float value = 1f;
+    float zOffset;
+    float zPos;
+    float theta = 0;
 
     private void Start()
     {
@@ -20,11 +25,16 @@ public class ColorSmoothCycle : MonoBehaviour
         startingColor = Color.HSVToRGB(startingHue, saturation, 1f);
         sr.color = startingColor;
         currentHue = startingHue;
+        offsetMagnitude = Random.Range(minOffset, maxOffset);
+        zPos = transform.position.z;
     }
 
     void Update()
     {
         currentHue = (currentHue + Time.deltaTime / secondsToCycle) % 1;
         sr.color = Color.HSVToRGB(currentHue, saturation, 1f);
+        zOffset = Mathf.Sin(theta) * offsetMagnitude;
+        theta += 2 * Mathf.PI / secondsToCycle * Time.deltaTime;
+        transform.position = new Vector3(transform.position.x, transform.position.y, zPos + zOffset);
     }
 }
